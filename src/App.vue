36aed7a14c5ef98406dc82aa16453556a8bbe8f3
xavir <xaviranik.me@gmail.com>
2021-04-14 14:53:30 +0600

    add: chat window, sidebar shared files

--- src/App.vue
+++ src/App.vue
@@ -6,127 +6,8 @@
       <div class="w-1/4 p-8">
         <Sidebar />
       </div>
-      <div class="w-1/2 bg-gray-100 overflow-y-auto rounded-lg p-8">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab expedita
-          consequatur accusamus nulla dolore consectetur vel repellendus quaerat
-          explicabo a corporis, vero, maiores dolorum. Quas dolorem nostrum
-          harum qui quod. Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Eos itaque impedit repellat consequatur similique nulla animi
-          perspiciatis asperiores fuga perferendis, excepturi hic optio minus
-          nisi quaerat ex tempore est blanditiis esse labore ad earum quas?
-          Iusto, iste possimus debitis quasi sit dolores dolorem totam nesciunt
-          in rem quos similique obcaecati quam repellendus distinctio suscipit
-          nihil. Quibusdam repudiandae, nobis nam voluptatibus in voluptatum aut
-          maxime iste laboriosam, recusandae id officiis quo doloremque mollitia
-          eaque totam? Quod neque dolorem iste cum facilis ipsam nam enim,
-          assumenda, distinctio esse, sunt magni animi? Autem amet illo odio
-          tempora quia neque expedita aut mollitia, quam nihil aperiam ex
-          magnam. Mollitia, laborum quam quos quaerat odio totam, et blanditiis
-          fuga debitis molestias corrupti esse quis commodi? Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Dolore voluptates temporibus
-          atque delectus fuga consequatur, tenetur placeat quam, ad eius facilis
-          illo quidem velit dolor, ullam provident necessitatibus soluta
-          explicabo eum praesentium sapiente ducimus quo! Provident sit
-          voluptate blanditiis, repellendus libero quasi facere eius. Iure quas
-          obcaecati pariatur. Impedit officia, distinctio magnam accusantium
-          fugit molestias voluptatem dolore illum perferendis illo est
-          repellendus, tempora omnis? Voluptatum nobis iste numquam amet? Nisi
-          laboriosam eos delectus eius optio sapiente ex molestiae? Nam sunt
-          impedit omnis soluta maxime possimus aspernatur tempora, corrupti
-          aliquid, voluptatem nisi. Nihil reprehenderit libero exercitationem!
-          Repellat sit voluptas sunt aspernatur incidunt quos perferendis vel
-          veritatis alias at, molestias minus quam cumque error ratione.
-          Deleniti quo modi est rem placeat? Magnam optio a dolorum saepe,
-          temporibus veniam repellat perspiciatis necessitatibus. Ad similique
-          deleniti aliquam, labore optio assumenda commodi earum reprehenderit
-          harum nemo provident alias consequuntur atque impedit, natus quos illo
-          quo doloribus iure porro error, totam magni? Harum, sit quia. Eum
-          perferendis eaque quam quos quo cumque nesciunt. Accusantium voluptate
-          dolorum esse sed doloremque iste aliquid natus soluta laborum ad.
-          Tempora, culpa quo aliquam fugit alias illum pariatur facere nam ipsa
-          quas cum perferendis magni non dicta quam explicabo illo nihil
-          laboriosam totam consectetur. Voluptatum suscipit consequatur autem
-          voluptates quidem laudantium cumque delectus illum optio alias commodi
-          est nemo eos fugit, incidunt quasi quo quod quam maiores asperiores ex
-          blanditiis deserunt iusto. Illo fuga sequi laboriosam nam recusandae
-          facere dolorem debitis nisi nihil, cupiditate, esse quam sed obcaecati
-          aperiam quae rerum, accusamus quod. Nam unde sed reprehenderit
-          cupiditate maxime similique, sunt deleniti a odio, officiis quibusdam.
-          Distinctio dicta cupiditate, expedita, dignissimos dolores esse et,
-          eveniet quasi ullam facere laboriosam exercitationem fuga beatae
-          repudiandae eligendi architecto. Qui, blanditiis? Ullam facilis quis
-          repellendus repellat, hic, quisquam esse iste inventore natus sint
-          quam maxime sequi. Fuga nostrum ullam rem ipsam vel reiciendis officia
-          omnis maxime explicabo expedita, cumque, ad accusamus! Cumque in
-          voluptates dolorem minima dicta, repudiandae fugiat deserunt quasi
-          optio corporis sed error consequatur perferendis totam! Nemo aliquid
-          cum repellendus distinctio dolor enim? Natus, quis corporis sunt rem
-          amet cumque aliquid molestias repellendus eligendi! Neque ea
-          voluptatibus magnam consectetur tempora ipsa suscipit rerum numquam id
-          eligendi asperiores modi, sit fugiat sunt nam. Alias aperiam quod,
-          fugit dolore ea, omnis aliquam reiciendis quibusdam laboriosam ullam
-          natus illo! Consequatur accusamus iusto quaerat, doloremque aspernatur
-          magnam molestias ad? Eveniet inventore nesciunt rerum labore magnam
-          qui iusto mollitia libero, accusamus dolor, ipsum accusantium eligendi
-          praesentium alias laborum optio repellat voluptate reprehenderit
-          laboriosam aut dolores necessitatibus porro quasi sapiente. Maiores
-          eos vitae sit. Accusantium amet debitis vitae id saepe voluptatum
-          atque consequuntur quia assumenda necessitatibus perspiciatis
-          consequatur repellat voluptas, placeat sunt cumque, vero excepturi
-          molestias magnam a. Sed iusto consectetur optio expedita at obcaecati
-          qui natus dolorem aliquam quo quam sint ab nesciunt, soluta magni
-          explicabo omnis sequi voluptas. Recusandae a unde natus ullam id,
-          doloribus perspiciatis! Aperiam beatae, quasi eius enim earum ducimus
-          similique libero animi excepturi, quae ipsum id, quam eaque ullam!
-          Libero reprehenderit mollitia error in! Esse, ipsam ducimus!
-          Voluptatibus voluptates culpa debitis animi quae ipsa, voluptate
-          perferendis quaerat ut. Nostrum id voluptatum suscipit voluptatem
-          reprehenderit architecto blanditiis atque laudantium tenetur cum ipsa
-          mollitia nesciunt, repellat voluptate, eaque unde illum. Explicabo
-          deleniti beatae aperiam quaerat quos exercitationem provident voluptas
-          recusandae eveniet ullam, necessitatibus eos numquam possimus
-          reprehenderit rerum totam, tenetur eum quidem modi sunt, eligendi ea.
-          Ratione doloremque provident praesentium ex quos quae, maxime
-          assumenda ab laborum reprehenderit explicabo consequatur esse illum
-          iure quibusdam voluptas saepe unde et vero molestias voluptatum!
-          Voluptate optio est odit vero quaerat quas magni, laudantium saepe
-          esse dicta odio? Delectus laudantium nihil ad atque assumenda aliquid
-          aspernatur eaque modi, velit eos, quis odio obcaecati molestias
-          pariatur exercitationem. Corrupti, commodi optio similique, impedit
-          voluptatibus iure iusto cumque quidem perspiciatis explicabo
-          recusandae ea laudantium aliquid dolore, laborum obcaecati delectus
-          deserunt porro in a rem accusamus! Commodi animi facilis quis porro a
-          mollitia quos dignissimos reiciendis, voluptatum velit reprehenderit
-          sapiente. Tenetur alias laudantium facere perferendis placeat sit
-          odio, voluptatibus doloremque nihil illo ut consectetur eos iste nobis
-          aspernatur, asperiores tempora, fugit eum accusamus velit saepe cum?
-          Pariatur consequuntur at error ea atque praesentium, quos quo aliquid
-          provident beatae ipsum, quae, saepe amet labore tempora ipsa deleniti
-          dolorum! Dolor assumenda, odit reprehenderit a sunt quae temporibus,
-          aliquid fugit ab hic architecto. Voluptate, aperiam? Quod dolorum
-          molestiae provident, labore quas repellat dolor iusto porro assumenda
-          ea, incidunt, laborum fugit. Consequuntur molestias iure laudantium,
-          explicabo odio iusto cumque mollitia atque ducimus, voluptatibus
-          accusantium corrupti quisquam vitae autem pariatur delectus dolores?
-          Eveniet cupiditate vel dignissimos optio. Veritatis, cupiditate.
-          Veniam minima laboriosam nihil eius cumque iusto, dolores alias odio
-          nobis dignissimos, recusandae placeat at dolore deleniti saepe?
-          Consectetur beatae est, similique perferendis temporibus expedita nemo
-          optio dolorem provident repudiandae tempora ab architecto molestias
-          doloremque velit vitae porro voluptatibus quas accusamus harum,
-          laborum fugiat nobis? Nesciunt iste quo voluptatem ab. Magnam, dolor
-          a! Id iure minus fuga modi molestiae, pariatur qui veritatis, vel
-          commodi molestias esse delectus recusandae a illum facilis nobis
-          perferendis! Nobis delectus ut aspernatur, voluptatem dolore dolorum
-          enim, accusantium iure sunt harum corrupti! Facilis commodi possimus
-          eos excepturi impedit reprehenderit, modi soluta nobis voluptatum
-          eaque quis blanditiis. Ratione assumenda cum aspernatur molestiae
-          numquam saepe odio quisquam doloribus necessitatibus ad quas suscipit
-          quasi, magnam molestias perspiciatis rem nemo, illo nam! Architecto,
-          perspiciatis impedit quasi natus nesciunt ipsa nam, repellendus fugit
-          molestias asperiores sunt, numquam mollitia earum non!
-        </p>
+      <div class="w-1/2 bg-gray-100 rounded-lg">
+        <ChatWindow />
       </div>
       <div class="w-1/4 p-8">
         <ChatBlock />
@@ -138,12 +19,14 @@
 <script>
 import Sidebar from "@/components/Sidebar";
 import ChatBlock from "@/components/ChatBlock";
+import ChatWindow from "@/components/ChatWindow";
 
 export default {
   name: "App",
   components: {
     Sidebar,
     ChatBlock,
+    ChatWindow,
   },
 };
 </script>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="border-b-2 border-gray-100 pb-4">
-      <h1 class="text-4xl font-medium text-pink-600">
+      <h1 class="text-4xl font-bold text-pink-600">
         <span>Aid</span><span class="text-pink-600 font-normal">Rock</span>
       </h1>
     </div>
@@ -60,6 +60,188 @@
             placeholder="Search"
             class="px-3 py-3 relative bg-gray-100 rounded-lg placeholder-gray-300 focus:outline-none w-full pl-10"
           />
+        </div>
+      </div>
+    </div>
+    <div class="mt-6">
+      <h1 class="text-lg font-semibold">Shared Files & Links</h1>
+      <div class="grid grid-cols-2 gap-4 mt-4">
+        <div
+          class="bg-green-100 px-4 py-6 rounded-lg shadow-md text-green-500 font-semibold"
+        >
+          <div class="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+              />
+            </svg>
+            <div class="ml-3">
+              <div class="text-xs">All Files</div>
+              <div class="text-4xl">120</div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="bg-gray-100 px-4 py-6 rounded-lg text-gray-500 font-semibold"
+        >
+          <div class="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8 text-gray-300"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="ml-3">
+              <div class="text-xs">All Links</div>
+              <div class="text-4xl">75</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="mt-6 w-full inline-flex items-center justify-between text-gray-400"
+      >
+        <div class="flex items-center text-gray-600 text-base font-semibold">
+          <div class="flex items-center">
+            <div class="bg-green-100 p-2 rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 text-green-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </div>
+            <div class="mx-3 text-sm">All Files</div>
+            <div class="bg-green-100 p-2 rounded-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 text-green-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 ml-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between mt-4">
+        <div class="flex items-center">
+          <div class="bg-yellow-100 px-4 py-3 rounded-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 text-yellow-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div class="ml-4">
+            <div class="text-sm font-semibold">Documents</div>
+            <div class="mt-1 text-xs">126 files, 268 MB</div>
+          </div>
+        </div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="flex items-center justify-between mt-4">
+        <div class="flex items-center">
+          <div class="bg-blue-100 px-4 py-3 rounded-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 text-blue-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div class="ml-4">
+            <div class="text-sm font-semibold">Images</div>
+            <div class="mt-1 text-xs">2226 files, 868 MB</div>
+          </div>
+        </div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
         </div>
       </div>
     </div>
