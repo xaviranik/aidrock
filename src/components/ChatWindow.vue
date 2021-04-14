b79f34ad5d0be2dfd522260dc8e1f95b37a2d30a
xavir <xaviranik.me@gmail.com>
2021-04-14 16:06:07 +0600

    add: chat window message box

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,95 +1,80 @@
 <template>
   <div class="h-full relative">
     <div
-      class="absolute top-0 left-0 w-full bg-gray-100 bg-opacity-90 backdrop-filter backdrop-blur-sm border-b-2 border-gray-200 py-8"
+      class="absolute top-0 left-0 w-full rounded-lg bg-white bg-opacity-90 backdrop-filter backdrop-blur-sm border-2 border-gray-100 py-4 px-4"
     >
-      Chat Window
+      <div class="flex items-center justify-between text-sm font-medium">
+        <div class="flex items-center ml-2">
+          <div class="font-semibold text-xl">Ticket #12542</div>
+          <div
+            class="inline text-xs px-2 py-1 ml-4 rounded-md text-white bg-green-500"
+          >
+            Open
+          </div>
+        </div>
+        <div class="flex items-center">
+          <div class="bg-pink-100 text-pink-600 px-4 py-2 rounded-lg shadow">
+            Messages
+          </div>
+          <div class="rounded-lg ml-4 px-4 py-2">Participants</div>
+        </div>
+      </div>
     </div>
-    <div class="h-full overflow-y-auto">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum sint iure
-      ex neque? Porro consequuntur et dicta voluptatibus! Rem ipsa deleniti
-      quibusdam accusantium. Quisquam laboriosam sint delectus magni veniam
-      eligendi nisi veritatis repudiandae recusandae illum. Ut consequuntur
-      aliquam facilis corporis molestiae illo hic, iusto quidem omnis officia
-      cum repudiandae odio. Minima aspernatur quaerat dolores ipsa
-      exercitationem recusandae ex id sit quisquam, sequi, quas ab est
-      voluptatibus aliquid repudiandae numquam nulla tempora officiis obcaecati,
-      fugiat eos laborum veritatis odio quis. Cum nihil expedita blanditiis
-      vitae, quibusdam molestias tempore, non qui ratione excepturi quis et
-      eveniet impedit. Eligendi quasi, distinctio, adipisci suscipit consectetur
-      animi aperiam ea nisi quis, quia modi? Veniam placeat tempora iusto culpa
-      molestias, animi commodi libero nam. Harum voluptate reiciendis quidem
-      quas assumenda eum libero, consectetur quis ipsum repellat ducimus
-      debitis, placeat veritatis? Provident repellat reiciendis dolorem
-      similique aliquam commodi qui tenetur, voluptas corporis nam optio libero
-      unde non numquam eligendi nisi beatae quam eveniet dolor iure, doloremque
-      esse nobis. Velit, quibusdam. Consectetur aut a, unde laborum laudantium
-      vitae ex numquam ut eligendi repellendus quo dolor cupiditate atque animi
-      non libero at asperiores odit delectus sequi deserunt ipsa, eveniet iure.
-      Sequi quis ab, nam similique eligendi porro dicta quisquam! Et, dolorem
-      repellat nisi atque optio voluptatem sunt consectetur quod suscipit,
-      temporibus voluptatum. Quis assumenda consectetur et! Ipsam quia sed
-      officia neque quasi tenetur illum fugiat! Nostrum libero, laudantium ut
-      nulla aperiam voluptas quod repellat magni facilis modi voluptates iure
-      doloribus velit tempora reprehenderit iste reiciendis ipsum officiis.
-      Accusamus incidunt blanditiis nemo rem ducimus dolores voluptatibus
-      veritatis reprehenderit quasi amet cupiditate voluptas laborum harum omnis
-      commodi quod qui aut numquam, est minima labore error soluta suscipit
-      totam? Tenetur temporibus aliquam a magnam nam voluptas quae id iure,
-      consectetur porro dolorum exercitationem laboriosam molestiae dolor
-      distinctio eius neque optio aspernatur fugiat earum facilis laudantium?
-      Voluptatem sunt necessitatibus inventore doloremque ipsam, vitae natus
-      dolores omnis amet itaque quasi quam officiis, aliquid, nesciunt facilis
-      quae molestiae! Quod culpa nihil ullam quaerat, dolores ipsa eum corporis
-      porro assumenda fugit quibusdam facilis distinctio alias perferendis rem
-      iure. Laborum, repellendus dolores, ex pariatur, voluptatem eum nostrum
-      tempora nobis voluptates repudiandae natus. Qui id nisi beatae omnis
-      neque, ducimus asperiores facere dolore nulla odit ipsam veritatis
-      possimus numquam ipsum facilis a, minima et, vel aperiam dignissimos
-      tempore officiis ullam molestiae magnam? Nulla deleniti ipsum itaque, vero
-      adipisci dignissimos necessitatibus debitis explicabo, sunt delectus amet
-      fugiat aut maiores, reiciendis harum culpa velit asperiores obcaecati
-      natus modi. Ipsum laborum consequuntur impedit laboriosam et error vitae
-      hic deserunt sit eligendi culpa quis expedita cumque nobis beatae ut
-      ratione omnis, aut quisquam tenetur totam id voluptatum fugit laudantium!
-      Nemo nam cumque quasi ut tempore qui sit cupiditate porro nobis officia
-      suscipit autem pariatur inventore, fugiat vel libero tenetur facere ab.
-      At, exercitationem quam earum, sapiente inventore facilis similique
-      suscipit cumque veniam beatae accusantium deleniti officia in odio,
-      cupiditate corporis dolores laudantium recusandae fuga illo. Sunt eaque
-      maxime quisquam hic iste id officia placeat voluptas minus aperiam,
-      consequuntur praesentium quaerat totam quidem, rerum odit culpa deleniti
-      distinctio natus, nisi error nulla. Temporibus ex distinctio quae odit
-      repellendus assumenda mollitia, praesentium, quis aspernatur eum ipsa
-      molestiae eos labore! Voluptas placeat omnis iure et voluptatibus esse
-      optio quasi porro error, eveniet cupiditate neque adipisci repellendus
-      illo autem, consequatur doloremque temporibus? Repudiandae unde ipsum non
-      ipsa repellendus asperiores aperiam vel qui sunt necessitatibus commodi,
-      ex porro distinctio architecto magni ea accusantium molestiae numquam
-      dolorem omnis! Sapiente, vero natus? Reprehenderit nobis nisi consectetur
-      necessitatibus tempora sunt, temporibus natus, illo minima iusto
-      repellendus. Adipisci, quasi id, aliquam eos voluptatem nemo sint voluptas
-      est ad placeat eum magni vitae, commodi animi reiciendis! Corporis
-      aspernatur mollitia accusamus. Nostrum, autem. Labore odio porro id eaque
-      minus, alias ratione dolor voluptatibus provident iusto, commodi sit,
-      eveniet molestiae amet voluptate animi ipsa incidunt vel inventore
-      perspiciatis sunt accusamus beatae. Quis libero ducimus assumenda ab vel
-      laborum perferendis maxime optio distinctio tempora cupiditate iusto
-      eveniet facilis, voluptatum ea mollitia laboriosam praesentium, dolores
-      rem illo? Distinctio blanditiis eveniet sint fugiat asperiores repellat
-      ullam ducimus eaque magni quam ex esse pariatur hic, ipsum odio, autem
-      alias soluta et facere numquam aut, totam sapiente optio expedita? Totam,
-      voluptatibus similique animi harum corporis iusto assumenda quisquam illum
-      alias temporibus non at tempora vero quia eligendi. Atque odio dolor, illo
-      culpa vero quae nam in sint quasi! Illo vero quos voluptate assumenda
-      earum in tenetur voluptates magni voluptas, veniam libero numquam,
-      provident facilis quod deserunt enim id ut consectetur minus suscipit vel!
-      Sit ratione, commodi quidem harum nobis perspiciatis similique sequi et
-      velit nulla assumenda? Quos beatae repellendus rem earum suscipit?
-      Consequatur eius eveniet odit a culpa asperiores. Officia natus quidem
-      nesciunt alias beatae reiciendis, cum atque pariatur vel iusto consectetur
-      unde. Minima veniam labore nam minus harum, ipsam, doloribus quae non
-      accusamus, perferendis quas aliquam suscipit fugiat.
+    <div class="h-full rounded-lg px-6 overflow-y-auto pt-24">
+      tio id maxime corporis voluptate vero, facilis, voluptatem vel!
+    </div>
+    <div
+      class="absolute bottom-0 mr-4 left-0 w-full rounded-b-lg bg-white border-2 border-gray-100 py-4 px-4"
+    >
+      <div class="flex items-center justify-between text-sm text-gray-400">
+        <input
+          type="text"
+          placeholder="Write your message..."
+          class="placeholder-gray-400 focus:outline-none w-full text-gray-900"
+        />
+        <div class="flex items-center ml-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <svg
+            class="h-5 ml-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+            ></path>
+          </svg>
+
+          <div class="ml-4 p-3 bg-pink-600 rounded-full text-white">
+            <svg
+              class="h-5"
+              fill="currentColor"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.75 17.612v4.638a.751.751 0 001.354.444l2.713-3.692zM23.685.139a.75.75 0 00-.782-.054l-22.5 11.75a.752.752 0 00.104 1.375l6.255 2.138 13.321-11.39L9.775 16.377l10.483 3.583a.753.753 0 00.984-.599l2.75-18.5a.751.751 0 00-.307-.722z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -98,5 +83,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
