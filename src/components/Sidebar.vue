c88df9104cc55c1541e7f90be518a34157876d91
xavir <xaviranik.me@gmail.com>
2021-04-15 21:52:18 +0600

    fix: online dropdown icon stroke

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -27,7 +27,7 @@
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              stroke-width="2"
+              stroke-width="3"
               d="M19 9l-7 7-7-7"
             />
           </svg>
